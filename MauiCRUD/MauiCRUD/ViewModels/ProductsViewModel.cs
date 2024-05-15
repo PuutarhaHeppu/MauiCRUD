bb7000d69a968053c5f78c7f226d3f394385c7c8
Opilane TTHK <opilane@tthk.ee>
2024-05-15 11:35:19 +0300

    f

--- MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
+++ MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
@@ -41,7 +41,7 @@ namespace MauiCRUD.ViewModels
                         Products.Add(product);
                     }
                 }
-            }, "Fetching products... ");
+            }, "Fetching products...");
         }
 
         private async Task ExecuteAsync(Func<Task> operation, string? busyText = null)
@@ -54,6 +54,7 @@ namespace MauiCRUD.ViewModels
             }
             catch (Exception)
             {
+
                 //throw;
             }
             finally
@@ -63,9 +64,67 @@ namespace MauiCRUD.ViewModels
             }
         }
 
-        [RelayCommand]
-        private void SetOperatingProducts(Product? product) => OperatingProduct = product ?? new()
-        
+        [ICommand]
+        private void SetOperatingProduct(Product? product) => OperatingProduct = product ?? new();
+
+        [ICommand]
+        private async Task SaveProductAsync()
+        {
+            if (OperatingProduct is null)
+                return;
+
+            var (isValid, errorMessage) = OperatingProduct.Validate();
+            if (!isValid)
+            {
+                await Shell.Current.DisplayAlert("Validation Error", errorMessage, "Ok");
+                return;
+            }
+
+            var busyText = OperatingProduct.Id == 0 ? "Creating product..." : "Updating product...";
+            await ExecuteAsync(async () =>
+            {
+                if (OperatingProduct.Id == 0)
+                {
+                    await _context.AddItemAsync<Product>(OperatingProduct);
+                    Products.Add(OperatingProduct);
+                }
+                else
+                {
+                    if (await _context.UpdateItemAsync<Product>(OperatingProduct))
+                    {
+                        var productCopy = OperatingProduct.Clone();
+
+                        var index = Products.IndexOf(OperatingProduct);
+                        Products.RemoveAt(index);
+
+                        Products.Insert(index, productCopy);
+                    }
+                    else
+                    {
+                        await Shell.Current.DisplayAlert("Error", "Product updating error", "Ok");
+                        return;
+                    }
+                }
+                SetOperatingProductCommand.Execute(new());
+            }, busyText);
+        }
+
+        [ICommand]
+        private async Task DeleteProductAsync(int id)
+        {
+            await ExecuteAsync(async () =>
+            {
+                if (await _context.DeleteItemByKeyAsync<Product>(id))
+                {
+                    var product = Products.FirstOrDefault(p => p.Id == id);
+                    Products.Remove(product);
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Delete Error", "Product was not deleted", "Ok");
+                }
+            }, "Deleting product...");
+        }
 
     }
 }
